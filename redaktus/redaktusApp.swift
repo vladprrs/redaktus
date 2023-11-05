c95bcf3dab161cffa4d81aa60f28f16bd1c50340
vladpr <inbox@vladpr.com>
2023-11-05 17:56:43 +0100

    Initial Commit

---
+++ redaktus/redaktusApp.swift
@@ -0,0 +1,17 @@
+//
+//  redaktusApp.swift
+//  redaktus
+//
+//  Created by Владислав Прищепов on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct redaktusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
